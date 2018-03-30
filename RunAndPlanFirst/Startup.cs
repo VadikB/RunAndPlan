ba2a320fd307ff30f9ead1acb26baf7d57b9f6e1
Unknown <bogachevv@mail.ru>
2018-03-30 14:34:20 +0300

    LoadProject

---
+++ RunAndPlanFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RunAndPlanFirst.Startup))]
+namespace RunAndPlanFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
